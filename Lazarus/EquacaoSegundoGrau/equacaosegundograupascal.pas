7473e4c1b59610ac04337379941d9a96a68004af
Willian <willianyos13@gmail.com>
2022-06-27 07:27:47 -0300

    Added result

--- Lazarus/EquacaoSegundoGrau/equacaosegundograupascal.pas
+++ Lazarus/EquacaoSegundoGrau/equacaosegundograupascal.pas
@@ -47,31 +47,65 @@ begin
 end;
 
 procedure TForm1.CalcularClick(Sender: TObject);
-var w,x,y,z,temp,debug:Real;
+var v,w,x,y,z,temp,resultado1,resultado2:Real;
 begin
+  v:=2;
   w:= 4;
   x:= strtofloat(aeq.Text);
   y:= strtofloat(beq.Text);
   z:= strtofloat(ceq.Text);
 
-  {$asmmode intel}
+  {$ASMMODE intel}
   asm
     finit
     fld x
     fld z
     fld w
-    fmul
-    fmul
+    fmul st,st(1)
+    fmul st,st(2)
     fld y
     fld y
-    fmul
-    fxch
-    fsub
+    fmul st,st(1)
+    fsub st,st(2)
+    fsqrt
     fstp temp
+    fld v
+    fld x
+    fmul
+    fld temp
+    fld y
+    fldz
+    fsubr
+    fadd
+    fdiv
+    fstp resultado1
   end;
-
-  raiz1eq.Text:= floattostr(temp);
-  raiz2eq.Text:= floattostr(debug);
+  asm
+    finit
+    fld x
+    fld z
+    fld w
+    fmul st,st(1)
+    fmul st,st(2)
+    fld y
+    fld y
+    fmul st,st(1)
+    fsub st,st(2)
+    fsqrt
+    fstp temp
+    fld v
+    fld x
+    fmul
+    fld temp
+    fld y
+    fldz
+    fsubr
+    fsub
+    fdiv
+    fstp resultado2
+  end;
+    raiz1eq.Text:= floattostr(resultado1);
+    raiz2eq.Text:= floattostr(resultado2);
 end;
 
 procedure TForm1.AClick(Sender: TObject);
