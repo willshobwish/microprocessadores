64dc287af403995a9e94a1f15882fa6a7888176e
Willian Murayama <willianyos13@gmail.com>
2022-08-04 08:53:58 -0300

    Questão 1

---
+++ Lazarus/Questao1/questaol.pas
@@ -0,0 +1,65 @@
+unit QuestaoL;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, Controls, Dialogs, Forms, Graphics, StdCtrls, SysUtils;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  va, vb, vc: real;
+begin
+  va := 3.0;
+  vb := 5.0;
+  vc := 9.0;
+    {$asmmode intel}
+  asm
+           FINIT
+           FLD1
+           FLDZ
+           FLDPI
+           FLD     va
+           FMUL
+           FLD     vb
+           FSUB    ST,ST(3)
+           FSINCOS
+           FST     vc
+           FCHS
+           FADD    ST(2),ST
+           FDIV
+           FSTP    vb
+           FST     va
+           FSTP    vc
+  end;
+  edit1.Text := floattostr(va);
+  edit2.Text := floattostr(vb);
+  edit3.Text := floattostr(vc);
+end;
+
+end.
